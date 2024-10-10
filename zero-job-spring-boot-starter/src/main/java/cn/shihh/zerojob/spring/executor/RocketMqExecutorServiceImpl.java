c15989a258c3ff5843ba97b668dba16a2fe8b5cd
shihh <shihao1016@qq.com>
2024-10-10 19:22:44 +0800

    rocketmq执行器发送时设置消息的key

--- zero-job-spring-boot-starter/src/main/java/cn/shihh/zerojob/spring/executor/RocketMqExecutorServiceImpl.java
+++ zero-job-spring-boot-starter/src/main/java/cn/shihh/zerojob/spring/executor/RocketMqExecutorServiceImpl.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -25,16 +28,16 @@ public class RocketMqExecutorServiceImpl implements ExecutorService {
     public Boolean executeJob(Job job) {
         String mqDestination = job.getJobExecuteTarget();
         String jobParams = job.getJobParams();
-        Object o = job.getJobParamsType().converter(jobParams);
+        Message<Object> sendMessage = MessageBuilder.withPayload(job.getJobParamsType().converter(jobParams)).setHeader(RocketMQHeaders.KEYS, job.getJobKey()).build();
         try {
-            SendResult sendResult = template.syncSend(mqDestination, o);
+            SendResult sendResult = template.syncSend(mqDestination, sendMessage.getPayload());
 
-            log.info("[{}]同步消息[{}]发送结果[{}]", mqDestination, o, JSONObject.toJSON(sendResult));
+            log.info("[{}]同步消息[{}]发送结果[{}]", mqDestination, sendMessage.getPayload(), JSONObject.toJSON(sendResult));
 
             job.setJobStatus(JobStatus.SUCCEED);
         } catch (Exception e) {
             job.setJobStatus(JobStatus.FAILED);
-            log.error("[{}]同步消息[{}]发送失败", mqDestination, o, e);
+            log.error("[{}]同步消息[{}]发送失败", mqDestination, sendMessage.getPayload(), e);
         }
         return true;
     }
